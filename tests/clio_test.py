308d04f6c3c2695cc5a9a923904682c5416322dd
Ivan Ruiz Manuel <72193617+irm-codebase@users.noreply.github.com>
2025-03-23 08:04:19 +0100

    Update snakemake version

--- tests/clio_test.py
+++ tests/clio_test.py
@@ -33,7 +33,7 @@ def test_snakemake_all_failure(module_path):
 def test_snakemake_integration_testing(module_path):
     """Run a light-weight test simulating someone using this module."""
     assert subprocess.run(
-        "snakemake --use-conda --dry-run",
+        "snakemake --use-conda",
         shell=True,
         check=True,
         cwd=module_path / "tests/integration",
